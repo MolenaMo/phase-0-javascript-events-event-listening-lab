d4454dba9dd93b7c9107d4161fe69ef99f62d504
Monique Lena Moore <moniquelmoore06@gmail.com>
2022-04-26 13:29:36 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+
+
+//Below passes test but when browser reload, alert happens since function is directly invoked
+let input = document.getElementById('input')
 function addingEventListener() {
-}
+input.addEventListener('click', addingEventListener)
+    
+    alert('I was clicked')
+}addingEventListener()
+
+//better way: function is invoked upon event/action of 'click' , but doesn't pass test 
+// let input = document.getElementById('input')
+// function addingEventListener() {
+//     alert('I was clicked')
+// }
+// input.addEventListener('click', addingEventListener)
